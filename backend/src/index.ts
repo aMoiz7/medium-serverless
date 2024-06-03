00ce8ae241a29758029b76c15f717f5fda0f8861
abdul moiz hussain <moizh750@gmail.com>
2024-06-04 01:03:45 +0530

    ignore wrangler.toml file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/extension'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+ const prisma = new PrismaClient({
+  datasourceUrl :env.DATABASE_URL,
+ }).$extents(withAccelerate())
+ return c.text('Hellow Hono !')
 })
 
 export default app
